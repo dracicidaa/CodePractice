afc598f167bb3952760badfbdedb41190e21eac2
Episcopalian-of-the-Mountain-West <280468738+dracicidaa@users.noreply.github.com>
2026-05-12 15:01:29 -0600

    Added in the example problem from class. Thank you Python Institute!

--- DIY split()/main.py
+++ DIY split()/main.py
@@ -1,1 +1,13 @@
+#What follows is the example problem straight from my classwork
+def mysplit(strng):
+    #
+    # put your code here
+    #
 
+
+print(mysplit("To be or not to be, that is the question"))
+print(mysplit("To be or not to be,that is the question"))
+print(mysplit("   "))
+print(mysplit(" abc "))
+print(mysplit(""))
+#Credit to Python Institute!!
