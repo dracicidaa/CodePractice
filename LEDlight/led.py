79879444878b4437822351544617aeb1264fe6b3
Episcopalian-of-the-Mountain-West <280468738+dracicidaa@users.noreply.github.com>
2026-05-14 12:58:56 -0600

    Finished logic, added in try except blocks, should run fine.

--- LEDlight/led.py
+++ LEDlight/led.py
@@ -73,3 +73,39 @@ num6 = led_states[6]
 num7 = led_states[7]
 num8 = led_states[8]
 num9 = led_states[9]
+
+running = True
+
+#Main program body
+while running:
+    selection - input('Please enter a number, or any negative number to exit program: ')
+    try:
+        if int(selection) >= 0:
+            for i in selection:
+                if int(i) == 0:
+                    print(num0, end=' ')
+                elif int(i) == 1:
+                    print(num1, end=' ')
+                elif int(i) == 2:
+                    print(num2, end=' ')
+                elif int(i) == 3:
+                    print(num3, end=' ')
+                elif int(i) == 4:
+                    print(num4, end=' ')
+                elif int(i) == 5:
+                    print(num5, end=' ')
+                elif int(i) == 6:
+                    print(num6, end=' ')
+                elif int(i) == 7:
+                    print(num7, end=' ')
+                elif int(i) == 8:
+                    print(num8, end=' ')
+                else:
+                    print(num9, end=' ')
+        else:
+            print('Good bye!')
+            running = False
+
+    except Exception:
+        print('Please enter a number!')
+        continue
