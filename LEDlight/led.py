70c5b1609d03f6bfb33299e12fa6a361b1228418
Episcopalian-of-the-Mountain-West <280468738+dracicidaa@users.noreply.github.com>
2026-05-14 12:44:51 -0600

    Defined a bunch of stuff!

--- LEDlight/led.py
+++ LEDlight/led.py
@@ -1,1 +1,75 @@
+#Pactice using lists, mulilines, etc to make an "LED" board
 
+#define stuff
+led_states = ['''
+###
+# #
+# #
+# #
+###
+''','''
+#
+#
+#
+#
+#
+''','''
+###
+  #
+###
+#
+###
+''','''
+###
+  #
+###
+  #
+###
+''','''
+# #
+# #
+###
+  #
+  #
+  ''','''
+###
+#
+###
+  #
+###
+''','''
+###
+#
+###
+# #
+###
+''','''
+###
+  #
+  #
+  #
+  #
+''','''
+###
+# #
+###
+# #
+###
+''','''
+###
+# #
+###
+  #
+###
+'''  
+]
+num0 = led_states[0]
+num1 = led_states[1]
+num2 = led_states[2]
+num3 = led_states[3]
+num4 = led_states[4]
+num5 = led_states[5]
+num6 = led_states[6]
+num7 = led_states[7]
+num8 = led_states[8]
+num9 = led_states[9]
