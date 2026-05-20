18c42b9ea91ecd725a367c9a06f18dc1ce283287
Episcopalian-of-the-Mountain-West <280468738+dracicidaa@users.noreply.github.com>
2026-05-20 09:02:56 -0600

    Wrote the program, and a super slick single line generator

---
+++ Digit of life/digitOfLife.py
@@ -0,0 +1,9 @@
+#The digit of life is a digit evaluated using somebodys birthday. Simply sum all digits. If result has more than one digit, continue summation.
+#For the time being, I will only allow integer inputs for the dates
+#I came up with this super slick single line input, generator. It takes a users input, filters out non numberical characters, and sums it all together
+
+lifeDigits = sum(int(char) for char in input('Pleas input your birthday here using only numbers (04/28/2000 instead of april 28 2000)> ') if char.isalnum())
+
+lifeDigits = sum(lifeDigits)
+
+print('Your life digit is: ', lifeDigits)
