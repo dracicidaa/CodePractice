60988826abd7a11e66b9486b115965da11f48a3f
Episcopalian-of-the-Mountain-West <280468738+dracicidaa@users.noreply.github.com>
2026-05-20 07:19:40 -0600

    Refactored program
    Total rewrite of the algorithm. I could have done better, I think, but I am happy that it is working properly. With minimal help! That ceasar cipher class I wrote was quite a bit of debugging.

--- Palindrome/palindrom.py
+++ Palindrome/palindrom.py
@@ -9,17 +9,17 @@
 sentnumb = 0
 senttotal = 0
 sentance = input('Please enter a potential palindrom here> ')
-senlen = len(sentance)
+sentlett = [char for char in sentance if char.isalpha()]
+senlen = len(sentlett) - 1
 
-for i in range(len(sentance)):
+for i in range(len(sentlett)):
     if i == senlen:
         break
-    if sentance(i) == sentance(senlen):
+    if sentlett[i] == sentlett[senlen]:
         senttotal += 1
-    senlen -= senlen
+    senlen -= 1
 
-if senttotal == (senlen // 2):
-    print('This is a palindrom.')
+if senttotal == (len(sentlett) // 2):
+    print('This is a palindrome.')
 else:
-    print('This is not a palindrome.')
-
+    print('this is not a palindrome.')
