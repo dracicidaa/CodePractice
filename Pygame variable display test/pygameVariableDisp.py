92f1869116e53d037ec40f05fc5721fc9aff9398
dracicidaa <280468738+dracicidaa@users.noreply.github.com>
2026-05-20 19:18:54 -0600

    Some changes to my setup, initial pygameVariableDisplay generic program setup

---
+++ Pygame variable display test/pygameVariableDisp.py
@@ -0,0 +1,7 @@
+#The purpose of this code is to figure out how to render text and images to a display
+#resolution agnostic
+#Default screen resolution will be 1920 x 1080 internally, but should correctly size to whatever
+#the monitor resolution is
+
+#importations
+import sys, pygame
