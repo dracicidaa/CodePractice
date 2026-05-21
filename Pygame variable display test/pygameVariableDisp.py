21c8b1557e2067f8f56124080acef0280d3ae87e
dracicidaa <280468738+dracicidaa@users.noreply.github.com>
2026-05-20 19:46:31 -0600

    Finished the very very basic screen routine

--- Pygame variable display test/pygameVariableDisp.py
+++ Pygame variable display test/pygameVariableDisp.py
@@ -8,16 +8,45 @@ import sys, pygame
 
 pygame.init()
 
-#setting internal resolution
+#setting internal resolution and the internal screen surface
 int_width = 1920
 int_height = 1080
+int_canvas = pygame.Surface((int_width, int_height))
 
 #setting initial window size, can be ignored when drawing full screen
 window_width = 640
 window_height = 480
+screen = pygame.display.set_mode((window_width, window_height), pygame.RESIZABLE)
 
 #Can put whatever else is needed for the program
 
 #initialize variables
 running = True
+
+while running:
+    for events in pygame.event.get():
+        if event == pygame.QUIT:
+            running = False
+        elif event.type == pygame.VIDOERESIZE:
+            #exclusively for windowed applications, can safely be ignored
+            window_width, window_height = event.w, event.h
+            screen = pygame.display.set_mode((window_width, window_height), pygame.RESIZABLE)
+        
+        #Drawing stuff starts here
+        #blank canvas
+        int_canvas.fill((0, 128, 128))
+
+        #Draw the internal canvas, drawn in relation to the internal screen not the actual screen
+        pygame.draw.rect(int_canvas, (255, 100, 100), (100, 100, 200, 150))
+
+        #resolution scaling
+        scaled_surface = pygame.transform.scale(int_canvas, (window_width, window_height))
+
+        screen.blit(scaled_surface, (0,0))
+
+        pygame.display.flip()
+        clock.tick(60)
+
+pygame.quit()
+sys.quit()
 
